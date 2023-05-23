d6e1f37fc1223bfa6415da8227c1b8d44917bbcd
karthick938 <133762682+karthick938@users.noreply.github.com>
2023-05-23 18:48:29 +0530

    Update Exexution2.java

--- src/test/java/Package1/Exexution2.java
+++ src/test/java/Package1/Exexution2.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+//import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Exexution2 {
 
