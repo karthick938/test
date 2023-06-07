e36552edec0421e4251f23e88620eef7a45c7103
karthick938 <133762682+karthick938@users.noreply.github.com>
2023-06-07 20:38:40 +0530

    Update Launch1.java

--- src/test/java/Package1/Launch1.java
+++ src/test/java/Package1/Launch1.java
@@ -31,6 +31,12 @@ public class Launch1 {
 		WebDriver driver = new RemoteWebDriver(url,capability);
 		driver.get("https://13-15-qa-1.myhbx.org");
 		Thread.sleep(5000);
+		driver.findElement(By.id("id_username")).sendKeys("superuser@hbxcloud.com");
+		driver.findElement(By.id("id_password")).sendKeys("02134B0dge@!");
+		driver.findElement(By.xpath("signin")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("/html/body/div/div[2]/div[2]/div[1]/a")).click();
+		
 		driver.quit();
 
 	}}
